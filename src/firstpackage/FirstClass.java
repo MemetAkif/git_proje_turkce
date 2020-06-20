e918f281be2967d1c12505e57ac4fc0320cc7e9d
Mehmet <haciarifyildiz@gmail.com>
2020-06-20 20:35:34 +0200

    ikinci mommit local repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 
      System.out.println("first commit from local repository");
+     System.err.println("second commit form local repository");
 
 	}
 
