c1882476983d4e2b9696c2ea470d4c88f497f356
Mehmet <haciarifyildiz@gmail.com>
2020-06-20 20:45:10 +0200

    ucuncu commit local repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,7 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 
      System.out.println("first commit from local repository");
-     System.err.println("second commit form local repository");
+     System.err.println("second commit from local repository");
+     System.err.println("third commit from local repository");
 
 	}
 
